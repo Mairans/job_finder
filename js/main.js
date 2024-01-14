0f7e53c798026e9c27ebccb199c984e2cf8a2fff
Mairans <142796352+Mairans@users.noreply.github.com>
2024-01-14 22:00:34 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-$(function () {
-  var mixer = mixitup('.vacantion__list');
-  $('.vacantion__filter-btn').on('click', function () {
-    $('.vacantion__filter-btn').removeClass('vacantion__filter-btn--active')
-    $(this).addClass('vacantion__filter-btn--active')
-  })
-})
+let offset = 0;
+const sliderList = document.querySelector('.slider__container');
+
+document.querySelector('.next').addEventListener('click', function() {
+  offset += 500 ;
+  if (offset > 3000) {
+    offset = 0;
+  }
+  sliderList.style.left = -offset +'px';
+});
+document.querySelector('.prev').addEventListener('click', function() {
+  offset -= 500 ;
+  if (offset < 0) {
+    offset = 3000;
+  }
+  sliderList.style.left = -offset +'px';
+});
